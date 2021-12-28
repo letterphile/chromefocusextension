c3c7deac1e431a28ee8af87e49e768a00aba4bf0
aswin4400 <aswin4400@gmail.com>
2021-12-28 15:28:15 +0000

    twitter filter is working now

--- content-script.js
+++ content-script.js
@@ -1,23 +1,32 @@
 const targetNode = document.body;
 const config = { attributes: false, childList: true, subtree: true };
+function removeCraps(){
 let images = document.getElementsByTagName('img')
-    let videos = document.getElementsByTagName('video')
+let longImages = document.getElementsByTagName('image')
+let videos = document.getElementsByTagName('video')
+let classImages = document.getElementsByClassName('r-1wyyakw')
+// let classVideos = document.getElementsByClassName('r-ipm5af')
     for (image of images){
         image.remove()
+    }
+    for (longImage of longImages){
+        longImage.remove()
     }
     for (video of videos){
         video.remove()
     }
+    for (classImage of classImages){
+        classImage.remove()
+    }
+
+    // for (classVideo of classVideos){
+    //     classVideo.remove()
+    // }
+}
+removeCraps()
 const observer = new MutationObserver(()=>{
 
-    let images = document.getElementsByTagName('img')
-    let videos = document.getElementsByTagName('video')
-    for (image of images){
-        image.remove()
-    }
-    for (video of videos){
-        video.remove()
-    }
+   removeCraps()
 });
 
 observer.observe(targetNode,config)
