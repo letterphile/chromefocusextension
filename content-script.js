2776d0a786baf92602ecb9444fd4477cc1f9f498
letterphile <aswin4400@gmail.com>
2022-01-07 23:27:37 +0000

    added whitelist

--- content-script.js
+++ content-script.js
@@ -7,8 +7,22 @@ let videos = document.getElementsByTagName('video')
 let classImages = document.getElementsByClassName('r-1wyyakw')
 
 // let classVideos = document.getElementsByClassName('r-ipm5af')
+let whiteListIg = ['lyitpgrs','lyit_literaturesociety',
+'lyit_indian_society','lyitinternational.society','lyitcareersservice','lyit_int',
+'careersportal.ie','mycareerplan.ie','lyit.su'];
 
-if(!document.location.href.includes('medium.com')){
+let whiteFlag = false;
+let i=0;
+const lenWhiteList = whiteListIg.length;
+
+while(i<lenWhiteList){
+    if(document.location.href.includes(whiteListIg[i])){
+        whiteFlag=true;
+        break;
+    }
+}
+
+if(!document.location.href.includes('medium.com') && !document.location.href.includes('whatsapp.com')){
     for (image of images){
         
         image.remove()
@@ -23,18 +37,25 @@ if(!document.location.href.includes('medium.com')){
         classImage.remove()
     }
 }
+else if (whiteFlag){
+
+}
 else{
  
-    let mediumProfileImages = document.querySelectorAll('.o.n')
-    let mediumProfilePageImages = document.getElementsByClassName('np')
+    //  let mediumProfileImages = document.querySelectorAll('')
+    let mediumProfilePageImages = document.getElementsByClassName('cx')
+    let whatsAppContactImages  = document.getElementsByClassName('_3GlyB')
+    for (whatsAppContactImage of whatsAppContactImages){
+        whatsAppContactImage.remove()
+    }
     for (mediumProfilePageImage of mediumProfilePageImages){
         mediumProfilePageImage.remove()
     }
-    for (mediumProfileImage of mediumProfileImages){
+    // for (mediumProfileImage of mediumProfileImages){
    
-        mediumProfileImage.remove()
+    //     mediumProfileImage.remove()
         
-    }
+    // }
     
 
 }
